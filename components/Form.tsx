8bfe08f50856e841fe6acd7153c0de9d1de76c8d
Will Pedroso <willyng.pedroso@gmail.com>
2022-09-18 17:08:32 -0300

    add focus on register function

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,10 +1,14 @@
+import { useRef } from "react";
 import { Button, Div, Input } from "./Styleds";
 
 export default function Form(props) {
 
+    const prodInput = useRef<HTMLInputElement>(null);
+
     const keyHandler = (e) => {
         if(e.key == 'Enter') {
             props.handleClickButton();
+            prodInput?.current.focus();
         }
     }
 
@@ -12,7 +16,7 @@ export default function Form(props) {
         <Div>
                 <div style={{ textAlign: 'center' }}>
                     
-                    <Input type="text" placeholder="Product" value={props.productName} onChange={e => props.setProductName(e.target.value)}></Input>
+                    <Input type="text" ref={prodInput} placeholder="Product" value={props.productName} onChange={e => props.setProductName(e.target.value)}></Input>
                     
                     <Input type="number" placeholder="Qtd" style={{ width: "5em" }} value={props.productQtd} onChange={e => props.setProductQtd(e.target.value)}></Input>
                     
