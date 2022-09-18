522f8058ca2dcf40fde0d1c3865cee6b832a27fc
Will Pedroso <willyng.pedroso@gmail.com>
2022-09-18 17:49:16 -0300

    update form component

--- components/Form.tsx
+++ components/Form.tsx
@@ -2,30 +2,30 @@ import { useRef } from "react";
 import { Button, Div, Input } from "./Styleds";
 
 export default function Form(props) {
-
+    
     const prodInput = useRef<HTMLInputElement>(null);
-
+    
     const keyHandler = (e) => {
         if(e.key == 'Enter') {
             props.handleClickButton();
             prodInput?.current.focus();
         }
     }
-
+    
     return (
         <Div>
-                <div style={{ textAlign: 'center' }}>
-                    
-                    <Input type="text" ref={prodInput} placeholder="Product" value={props.productName} onChange={e => props.setProductName(e.target.value)}></Input>
-                    
-                    <Input type="number" placeholder="Qtd" style={{ width: "5em" }} value={props.productQtd} onChange={e => props.setProductQtd(e.target.value)}></Input>
-                    
-                    <Input type="number" placeholder="R$" style={{ width: "5em" }} value={props.productPrice} onChange={e => props.setProductPrice(e.target.value)} onKeyUp={keyHandler}></Input>
+            <div style={{ textAlign: 'center' }}>
                 
-                </div>
-                <Div>
-                    <Button onClick={props.handleClickButton}>Register</Button>
-                </Div>
+                <Input type="text" ref={prodInput} placeholder="Product" value={props.productName} onChange={e => props.setProductName(e.target.value)}></Input>
+                
+                <Input type="number" placeholder="Qtd" style={{ width: "5em" }} value={props.productQtd} onChange={e => props.setProductQtd(e.target.value)}></Input>
+                
+                <Input type="number" placeholder="R$" style={{ width: "5em" }} value={props.productPrice} onChange={e => props.setProductPrice(e.target.value)} onKeyUp={keyHandler}></Input>
+            
+            </div>
+            <Div>
+                <Button onClick={props.handleClickButton}>Register</Button>
+            </Div>
         </Div>
     )
 }
